1ceb48a506a0bcecb7630223272fdce8f8a0072e
堀田優介 <yuusuke@horita-no-MacBook-Air.local>
2020-03-07 19:16:25 +0900

    meshiterro commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+Refile.secret_key = 'd737b71597908e6a3254b786dc0f1b7de2e175d72a199267005c4d3a62c2cac5e7fd40dda998a1d536525cbc00953b88f1893a021483edf178fcf3690bf9388a'
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
